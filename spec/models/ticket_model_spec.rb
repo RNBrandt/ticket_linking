20dec2a1f59b0ad0dc01d78a28bdffda1900f432
Reuben Brandt <Reubennbrandt@gmail.com>
2017-02-26 21:57:04 -0800

    add testing

---
+++ spec/models/ticket_model_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe Ticket, type: :model do
+  describe '#related' do
+    let(:tick) { Ticket.create() }
+    let(:tock) { Ticket.create() }
+    let(:clock) { Ticket.create() }
+    let(:tickets) { Ticket.all }
+
+    context 'when a ticket has no relations' do
+
+      it 'will return itself as its only relation' do
+        expect(tick.related tickets).to contain_exactly(tick)
+        expect(tock.related tickets).to contain_exactly(tock)
+        expect(clock.related tickets).to contain_exactly(clock)
+      end
+    end
+
+    context 'when a ticket is related to another ticket' do
+      before do
+        tick.update(incidents: [tock])
+        tock.update(problem: tick)
+      end
+
+      it 'will return an array of only the two related tickets' do
+        expect(tick.related tickets).to contain_exactly(tick, tock)
+        expect(tock.related tickets).to contain_exactly(tick,tock)
+      end
+    end
+
+    context 'when tickets are related linearly' do
+      before do
+        tick.update(incidents: [tock])
+        tock.update(problem: tick, incidents: [clock])
+        clock.update(problem: tock)
+      end
+
+      it 'each will return arrays with all related tickets' do
+        expect(tick.related tickets).to contain_exactly(tick, tock, clock)
+        expect(tock.related tickets).to contain_exactly(tick, tock, clock)
+        expect(clock.related tickets).to contain_exactly(tick, tock, clock)
+      end
+    end
+
+    context 'when tickets have branching (sibling) relationships' do
+      before do
+        tick.update(incidents: [tock, clock])
+        tock.update(problem: tick)
+        clock.update(problem: tick)
+      end
+
+      it 'will return arrays with all related tickets' do
+        expect(tick.related tickets).to contain_exactly(tick, tock, clock)
+        expect(tock.related tickets).to contain_exactly(tick, tock, clock)
+        expect(clock.related tickets).to contain_exactly(tick, tock, clock)
+      end
+    end
+  end
+end
